3d13be0275731da34f1ca1467aece4439e19aa12
J4nssent <tuur.janssen@gmail.com>
2022-12-03 16:39:03 +0100

    commit

---
+++ scrape_json.py
@@ -0,0 +1,116 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from datetime import date
+import pandas as pd
+import pgeocode
+import json
+import csv
+
+brands = [
+    'Mercedes-Benz', 'BMW', 'Toyota', 'Volkswagen', 'Audi',
+    'Renault', 'Subaru', 'Nissan', 'Porsche', 'Volvo', 'Land Rover', 'Mazda',
+    'Mitsubishi', 'Alfa Romeo', 'Lexus', 'Skoda', 'Honda'
+]
+
+# url of search results (page 1)
+query = "lst/{}?sort=price&asc=0&ustate=N,U&atype=C&cy=B&priceto=10000"
+
+# for getting distance
+distance = pgeocode.GeoDistance('BE')
+zip_code = '2910'
+
+# for getting age
+today = date.today()
+
+base_url = 'https://www.autoscout24.be/nl/'
+driver = webdriver.Chrome("chromedriver")
+
+# full json data for listings
+data = []
+
+for brand in brands:
+    # brand results url
+    url = base_url + query.format(brand.lower().replace(' ','-'))
+
+    # iterate pages
+    page = 1
+    while True:
+        driver.get(url + "&page=" + str(page))
+        elements = driver.find_elements(By.ID, '__NEXT_DATA__')
+        if not elements:
+            break
+        script = elements[0].get_attribute("innerHTML")
+        page_data = json.loads(script)
+        listings = page_data['props']['pageProps']['listings']
+
+        # if max page count is reached, start over with current price as minimum
+        if page == 20:
+            if len(listings) == 20:
+                price = listings[19]['prices']['public']['priceRaw']
+                url = base_url + query.format(brand.lower()) + "&pricefrom=" + str(price)
+                page = 1
+            else:
+                break
+        else:
+            page += 1
+
+        if listings:
+            data.extend(listings)
+        else:
+            break
+
+driver.close()
+
+# reduce json data to relevant data
+def handleListing(l):
+    try:
+        # calculate distance
+        listing_zip_code = l['location']['zip']
+        dis = distance.query_postal_code(zip_code, listing_zip_code)
+
+        # calculate vehicle age in days
+        reg_date_str = l['tracking']['firstRegistration']
+        if reg_date_str in ['new', 'unknown']:
+            reg_age = 0
+        else:
+            month, year = reg_date_str.rsplit('-')
+            reg_date = date(int(year), int(month), 1)
+            reg_age = (today - reg_date).days
+
+        # listing age in days
+        listing_date_str = l['createdTimestampWithOffset']
+        year, month, day = listing_date_str[:10].rsplit('-')
+        listing_date = date(int(year), int(month), int(day))
+        listing_age = (today - listing_date).days
+
+        # return as dict
+        return {
+            'guid': l['id'],
+            'make': l['vehicle']['make'],
+            'model': l['vehicle']['model'],
+            'vehicle-type': l['vehicle']['bodyType']['formatted'],
+            'seller-type': l['seller']['type'],
+            'price': l['prices']['public']['priceRaw'],
+            'fuel-type': l['tracking']['fuelType'],
+            'mileage': l['tracking']['mileage'],
+            'year': l['tracking']['firstRegistration'],
+            'img-url': l['images'][0],
+            'zip-code': listing_zip_code,
+            'distance': dis,
+            'reg-age': reg_age,
+            'listing-age': listing_age,
+        }
+    except:
+        return None
+
+formatted = [x for x in map(handleListing, data) if x is not None]
+
+# store as CSV
+fn = "".join(brands)
+df = pd.DataFrame(formatted) 
+df.to_csv("csv/" + fn + ".csv", index=False, encoding='utf-8')
+
+# store JSON data
+# file_name = query.replace('/', '').replace('?', '') + '.json'
+# file = open("json/" + file_name, 'a')
+# file.write(json.dumps(data))
